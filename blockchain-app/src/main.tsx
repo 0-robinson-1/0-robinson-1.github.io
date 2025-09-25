9aef9b7351430d2f244753759ffe07e595aa6a45
0.RobinSon.1 <0.robinson.1@protonmail.com>
2025-09-25 13:52:49 +0200

    edit css path + Vite rebuild

--- blockchain-app/src/main.tsx
+++ blockchain-app/src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { WalletProvider } from './contexts/WalletContext'
 import './index.css'
-import './css/styles.css'; // Import global styles
+import '../../css/styles.css'; // Import global styles
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
