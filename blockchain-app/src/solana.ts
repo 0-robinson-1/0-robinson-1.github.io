a8dfa9b9557693e8d2395748b0018fc52633bcfa
0.RobinSon.1 <0.robinson.1@protonmail.com>
2025-10-26 09:23:50 +0100

    Switching from Dev net to Test net

--- blockchain-app/src/solana.ts
+++ blockchain-app/src/solana.ts
@@ -2,8 +2,8 @@
 
 import { Connection, clusterApiUrl } from '@solana/web3.js';
 
-// RPC_ENDPOINT points at Solana Devnet (more reliable for airdrops)
-export const RPC_ENDPOINT = clusterApiUrl('devnet');
+// RPC_ENDPOINT points at Solana Testnet
+export const RPC_ENDPOINT = clusterApiUrl('testnet');
 
 // The shared connection object for all Solana calls
 export const connection = new Connection(RPC_ENDPOINT, 'confirmed');
